38709f23275f98f98032261e3fdcb05f358215ba
Hector J. Vasquez <ipi.vasquez@gmail.com>
2019-06-12 14:55:08 -0500

    Merge David into Ipi

--- lib/features/features_models.py
+++ lib/features/features_models.py
@@ -24,8 +24,25 @@ HARALICK_NAMES = [
     'sum var', 'sum ent', 'entropy', 'diff var', 'diff ent', 'IC I',
     'IC II'
 ]
+<< << << < Updated upstream
 hh_disc = list(map(lambda x: 'Disc ' + x, HARALICK_NAMES))
 hh_cup = list(map(lambda x: 'Cup ' + x, HARALICK_NAMES))
+== == == =
+
+HH_DISC = list(map(lambda x: 'Disc ' + x, HARALICK_NAMES))
+HH_CUP = list(map(lambda x: 'Cup ' + x, HARALICK_NAMES))
+HH_FULL = list(map(lambda x: 'Full ' + x, HARALICK_NAMES))
+
+# Calculating Haralick features
+# DEGREES = [0, 45, 90, 135, mean(calculated)]
+# Full images => Orientation: 90 degrees, distance: 2
+print(' => Calculating Haralick for the full images')
+_haralick_complete = pd.DataFrame([
+    mt.features.haralick(
+        cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), distance=2)[2]
+    for img in ds.original_images
+], columns=HH_FULL)
+>>>>>> > Stashed changes
 
 # Disc images => Orientation: 135 degrees, distance: 1
 print(' => Calculating Haralick for the disc images')
@@ -33,7 +50,7 @@ _haralick_disc = pd.DataFrame([
     mt.features.haralick(
         cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), distance=1)[3]
     for img in ds.discs
-], columns=hh_disc)
+], columns=HH_DISC)
 
 # Cup images => Orientation: 90 degrees, distance: 3
 print(' => Calculating Haralick for the cup images')
@@ -41,9 +58,9 @@ _haralick_cup = pd.DataFrame([
     mt.features.haralick(
         cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), distance=3)[2]
     for img in ds.cups
-], columns=hh_cup)
+], columns=HH_CUP)
 
-_haralick = pd.concat((_haralick_disc,_haralick_cup), axis=1)
+_haralick = pd.concat((_haralick_disc, _haralick_cup), axis=1)
 
 # CDR
 print(' => Calculating CDR')
@@ -52,8 +69,8 @@ _cdr = pd.DataFrame([
 ], columns=['CDR'])
 
 # Calculating form features
-cols_disc = ['Perimeter_disc','Area_disc','Compacity_disc','X centroid_disc','Y centroid_disc']
-cols_cup = ['Perimeter_cup','Area_cup','Compacity_cup','X centroid_cup','Y centroid_cup']
+cols_disc = ['Perimeter_disc', 'Area_disc', 'Compacity_disc', 'X centroid_disc', 'Y centroid_disc']
+cols_cup = ['Perimeter_cup', 'Area_cup', 'Compacity_cup', 'X centroid_cup', 'Y centroid_cup']
 print(' => Calculating form features')
 
 _form_disc = []
@@ -62,10 +79,10 @@ for i in range(len(ds.disc_masks)):
     _form_disc.append(form.form_descriptors(ds.disc_masks[i]))
     _form_cup.append(form.form_descriptors(ds.cup_masks[i]))
 
-_form_disc = pd.DataFrame(_form_disc, columns = cols_disc)
-_form_cup = pd.DataFrame(_form_cup, columns = cols_cup)
+_form_disc = pd.DataFrame(_form_disc, columns=cols_disc)
+_form_cup = pd.DataFrame(_form_cup, columns=cols_cup)
 
-_form = pd.concat((_form_disc,_form_cup), axis=1)
+_form = pd.concat((_form_disc, _form_cup), axis=1)
 
 
 # DF to store features
@@ -74,25 +91,35 @@ meta['ids'] = ds.ids
 meta['Diagnosis'] = ds.Y
 
 _models = [
-    pd.concat( (meta, pd.DataFrame(_cdr),pd.DataFrame(_haralick), pd.DataFrame(_form)) ,axis=1),
-    pd.concat( (meta, pd.DataFrame(_cdr)), axis=1), 
-    pd.concat( (meta, pd.DataFrame(_haralick)), axis=1), 
-    pd.concat( (meta, pd.DataFrame(_form)) , axis=1),
-    pd.concat( (meta, pd.DataFrame(_cdr),pd.DataFrame(_haralick)) ,axis=1),
-    pd.concat( (meta, pd.DataFrame(_cdr),pd.DataFrame( _form)) ,axis=1),
-    pd.concat( (meta, pd.DataFrame(_haralick),pd.DataFrame( _form)) ,axis=1)  
+    pd.concat((meta, pd.DataFrame(_cdr), pd.DataFrame(_haralick), pd.DataFrame(_form)), axis=1),
+    pd.concat((meta, pd.DataFrame(_cdr)), axis=1),
+    pd.concat((meta, pd.DataFrame(_haralick)), axis=1),
+    pd.concat((meta, pd.DataFrame(_form)), axis=1),
+    pd.concat((meta, pd.DataFrame(_cdr), pd.DataFrame(_haralick)), axis=1),
+    pd.concat((meta, pd.DataFrame(_cdr), pd.DataFrame(_form)), axis=1),
+    pd.concat((meta, pd.DataFrame(_haralick), pd.DataFrame(_form)), axis=1)
 ]
 
+
 def cdr():
-    return pd.concat((pd.DataFrame(_cdr),meta),axis=1)
+    return pd.concat((pd.DataFrame(_cdr), meta), axis=1)
+
+
 def haralick():
-    return pd.concat((pd.DataFrame(_haralick),meta),axis=1)
+    return pd.concat((pd.DataFrame(_haralick), meta), axis=1)
+
+
 def haralick_disc():
-    return pd.concat((pd.DataFrame(_haralick_disc),meta),axis=1)
+    return pd.concat((pd.DataFrame(_haralick_disc), meta), axis=1)
+
+
 def haralick_cup():
-    return pd.concat((pd.DataFrame(_haralick_cup),meta),axis=1)    
+    return pd.concat((pd.DataFrame(_haralick_cup), meta), axis=1)
+
+
 def haralick_complete():
-    return pd.concat((pd.DataFrame(_haralick_complete),meta),axis=1)
-    
+    return pd.concat((pd.DataFrame(_haralick_complete), meta), axis=1)
+
+
 def models():
     return _models
