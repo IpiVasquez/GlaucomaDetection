1733e24a06be3ef52393c5328cbd296d98564c33
Hector J. Vasquez <ipi.vasquez@gmail.com>
2019-06-12 23:38:13 -0500

    Metrics added to evaluation

--- lib/evaluator.py
+++ lib/evaluator.py
@@ -7,14 +7,26 @@ def evaluate(classifier, X, Y, params={}):
     loo = LeaveOneOut()
     preds = np.array([])
     for train_idx, test_idx in loo.split(X):
-        model = classifier(*params)
+        model = classifier(**params)
         model.fit(X[train_idx], Y[train_idx])
         preds = np.append(preds, model.predict(X[test_idx]))
 
-    mcc = matthews_corrcoef(Y, preds)
-    score = balanced_accuracy_score(Y, preds)
-
     return {
-        'MCC': mcc,
-        'Score': score
+        'MCC': matthews_corrcoef(Y, preds),
+        'Accuracy': (Y == preds).mean(),
+        'BAS': balanced_accuracy_score(Y, preds),
+        'BER': ber(Y, preds)
     }
+
+
+def ber(y_true, y_predicted):
+    """Calculates BER for classification."""
+    b = 0
+    classes = np.unique(y_true)
+    # Iterate over classes
+    for c in classes:
+        idx = y_true == c
+        c_prediction = y_predicted[idx]
+        c_true = y_true[idx]
+        b += (c_true != c_prediction).sum() / c_true.shape[0]
+    return b / classes.shape[0]
