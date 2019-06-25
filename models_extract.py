06587039fd195b6a86e59c56cb384470d5a023ef
Hector J. Vasquez <ipi.vasquez@gmail.com>
2019-06-25 13:29:20 -0500

    Remove unused

--- models_extract.py
+++
@@ -1,9 +0,0 @@
-from lib.features import features_models
-import pandas as pd
-models = features_models.models()
-
-ind = 1
-print(" => writing csv files")
-for m in models:
-    pd.DataFrame(m).to_csv('results/models/model{}.csv'.format(ind))
-    ind += 1
